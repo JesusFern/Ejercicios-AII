a118c4d2d6503f860753ce72969fd0ed66b3fc9a
JesusFern <jfr16se>
2024-10-10 16:56:27 +0200

    Ejercicio 4 finalizado

--- Ejercicios/ejercicio_bs4/ejercicio4.py
+++ Ejercicios/ejercicio_bs4/ejercicio4.py
@@ -43,7 +43,6 @@ def almacenar_bd():
             equipo_visitante = resultado.find_all("span", class_="nombre-equipo")[1].string.strip()
             marcador = resultado.find("td", class_="col-resultado finalizado").a.string.strip()
             enlace_detalles = resultado.find("td", class_="col-resultado finalizado").a['href']
-            print(enlace_detalles)
             conn.execute("INSERT INTO JORNADA (NUMERO_JORNADA ,EQUIPO_LOCAL,EQUIPO_VISITANTE,RESULTADO, ENLACE) \
              VALUES (?,?,?,?,?)", (numero_jornada, equipo_local, equipo_visitante, marcador, enlace_detalles))
             conn.commit()
@@ -81,15 +80,16 @@ def listar_jornadas():
 def buscar_por_jornada():
     def listar_por_numero_jornada(event):
         conn = sqlite3.connect('jornadas.db')
-        num = '% ' + w.get() + ''
+        num = '% ' + w.get()
         cursor_1 = conn.execute("SELECT NUMERO_JORNADA ,EQUIPO_LOCAL,EQUIPO_VISITANTE,RESULTADO from JORNADA WHERE NUMERO_JORNADA LIKE ?",  (num,))
         v = Toplevel()
         scrollbar = Scrollbar(v)
         scrollbar.pack( side = RIGHT, fill=Y )
         Lb1 = Listbox(v, width=150, yscrollcommand = scrollbar.set)
-        Lb1.insert(END, str(cursor_1.fetchone()[0]))
+        data = cursor_1.fetchall()
+        Lb1.insert(END, str(data[0][0]))
         Lb1.insert(END, "------------------------------------------------------")
-        for row in cursor_1:
+        for row in data:
             Lb1.insert(END, str(row[1]) + " " + str(row[3]) + " "+ str(row[2]))
         Lb1.pack(side = LEFT, fill = BOTH )
         scrollbar.config( command = Lb1.yview )
@@ -119,9 +119,10 @@ def estadisticas_jornada():
         cursor_1 = conn.execute("SELECT NUMERO_JORNADA ,EQUIPO_LOCAL,EQUIPO_VISITANTE,RESULTADO from JORNADA WHERE NUMERO_JORNADA LIKE ?",  (num,))
         v = Toplevel()
         Lb1 = Listbox(v, width=150)
-        Lb1.insert(END, str(cursor_1.fetchone()[0]))
+        data = cursor_1.fetchall()
+        Lb1.insert(END, str(data[0][0]))
         Lb1.insert(END, "------------------------------------------------------")
-        for row in cursor_1:
+        for row in data:
             lista_goles = str(row[3]).split("-")
             goles_local = int(lista_goles[0])
             goles_visitante = int(lista_goles[1])
@@ -153,6 +154,86 @@ def estadisticas_jornada():
     w = Spinbox(v, from_=1, to=num_jornadas)
     w.bind("<Return>", computar_estadisticas)
     w.pack()
+    
+def buscar_goles():
+    def actualizar_equipo_local():
+        conn = sqlite3.connect('jornadas.db')
+        num = '% ' + w1.get()
+        cursor_1 = conn.execute("SELECT EQUIPO_LOCAL from JORNADA WHERE NUMERO_JORNADA LIKE ?",  (num,))
+        eq_local = list()
+        for row in cursor_1:
+            eq_local.append(row[0])
+        conn.close()
+        w2.config(value= eq_local)
+        w2.delete(0, "end")
+        w2.insert(0, eq_local[0])
+        actualizar_equipo_visitante()
+    
+    def actualizar_equipo_visitante():
+        conn = sqlite3.connect('jornadas.db')
+        num = '% ' + w1.get()
+        eq = w2.get()
+        cursor_1 = conn.execute("SELECT EQUIPO_VISITANTE from JORNADA WHERE NUMERO_JORNADA LIKE ? AND EQUIPO_LOCAL LIKE ?",  (num,eq))
+        var.set(cursor_1.fetchone()[0])
+        conn.close()
+    
+    def buscar_goleadores():
+        conn = sqlite3.connect('jornadas.db')
+        local = w2.get()
+        jornada = '% ' + w1.get()
+        visitante = str(var.get())
+        cursor_enlace = conn.execute("SELECT ENLACE from JORNADA WHERE NUMERO_JORNADA LIKE ? AND EQUIPO_LOCAL = ? AND EQUIPO_VISITANTE = ?", (jornada,local,visitante))
+        enlace = cursor_enlace.fetchone()[0]
+        
+        f = urllib.request.urlopen(enlace)
+        s = BeautifulSoup(f, "lxml")
+        
+        goles_locales = ""
+        goles_visitantes = ""
+        
+        local_goals_info = s.find("div", class_="scr-hdr__scorers")
+        local_spans = local_goals_info.find_all('span', class_=lambda x: x != 'red-card')
+        for local_span in local_spans:
+            goles_locales = goles_locales + local_span.string
+            
+        visitor_goals_info = s.find("div", class_="scr-hdr__team is-visitor").find("div", class_="scr-hdr__scorers")
+        visitor_spans = visitor_goals_info.find_all('span', class_=lambda x: x != 'red-card')
+        for visitor_span in visitor_spans:
+            goles_visitantes = goles_visitantes + visitor_span.string
+            
+        v = Toplevel()    
+        Lb1 = Listbox(v, width=150)
+        Lb1.insert(END, local + " : " + goles_locales) 
+        Lb1.insert(END, visitante + " : " + goles_visitantes)
+        Lb1.pack(side = LEFT, fill = BOTH )
+        conn.close()
+        
+    conn = sqlite3.connect('jornadas.db')
+    cursor_j = conn.execute("SELECT COUNT(DISTINCT NUMERO_JORNADA) from JORNADA")
+    num_jornadas= cursor_j.fetchone()[0]
+    conn.close()
+    
+    v = Toplevel()
+    L1 = Label(v, text="Seleccione jornada: ")
+    L1.pack( side = LEFT, padx=5)
+    w1 = Spinbox(v, from_=1, to=num_jornadas ,command =actualizar_equipo_local)
+    w1.pack(side = LEFT, padx=5)
+    
+    L2 = Label(v, text="Seleccione equipo local: ")
+    L2.pack( side = LEFT, padx=5)
+    w2 = Spinbox(v, value=["Selecciona una jornada primero"], command = actualizar_equipo_visitante)
+    w2.pack(side = LEFT, padx=5)
+    
+    L3 = Label(v, text="Equipo visitante: ")
+    L3.pack( side = LEFT, padx=5)
+    var = StringVar()
+    L4 = Label(v, textvariable=var, relief=RAISED , width = 20)
+    var.set("Selecciona Equipo/Jornada")
+    L4.pack(side = LEFT, padx=5)
+    
+    B1 = Button(v, text ="Buscar goles", command=buscar_goleadores)
+    B1.pack(side = LEFT, padx=5)
+    
 def ventana_principal():
         
     root = Tk()
@@ -161,12 +242,13 @@ def ventana_principal():
     B2 = Button(root, text ="Listar jornadas", command = listar_jornadas)
     B3 = Button(root, text ="Buscar por jornada", command = buscar_por_jornada)
     B4 = Button(root, text ="Estadisticas jornada", command = estadisticas_jornada)
-    
+    B5 = Button(root, text ="Buscar goles", command = buscar_goles)
     
     B1.place(x=50,y=10)
     B2.place(x=50,y=40)
     B3.place(x=50,y=70)
     B4.place(x=50,y=100)
+    B5.place(x=50,y=130)
     root.mainloop()
     
     
